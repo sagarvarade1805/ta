4bee14936b3dd4c6fb5ac4857b56acbca8b54415
sagarvarade1805 <63959921+sagarvarade1805@users.noreply.github.com>
2020-06-25 22:14:28 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,27 @@
+$("#delete-link1").click(function(){
+    alert("You removed " +$("#name1").text()+" from this subject");
+    $(this).parent().parent().parent().remove();
+});
+$("#delete-link2").click(function(){
+    alert("You removed " +$("#name2").text()+" from this subject");
+    $(this).parent().parent().parent().remove();
+});
+$("#delete-link3").click(function(){
+    alert("You removed " +$("#name3").text()+" from this subject");
+    $(this).parent().parent().parent().remove();
+});
+$("#delete-link4").click(function(){
+    alert("You removed " +$("#name4").text()+" from this subject");
+    $(this).parent().parent().parent().remove();
+});
+$("#delete-link5").click(function(){
+    alert("You removed " +$("#name5").text()+" from this subject");
+    $(this).parent().parent().parent().remove();
+});
+$("#delete-link6").click(function(){
+    alert("You removed " +$("#name6").text()+" from this subject");
+    $(this).parent().parent().parent().remove();
+});
+$("#butt").click(function(){
+    alert("Every Student is added");
+});
